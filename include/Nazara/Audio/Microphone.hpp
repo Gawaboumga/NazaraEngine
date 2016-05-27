61c8b53a4b69e7f20a6ee4d792739df984c3a092
Gawaboumga <yourihubaut@hotmail.com>
2016-05-27 12:03:25 +0200

    Forget to comment those headers

---
+++ include/Nazara/Audio/Microphone.hpp
@@ -0,0 +1,23 @@
+// Copyright (C) 2015 Jérôme Leclercq
+// This file is part of the "Nazara Engine - Audio module"
+// For conditions of distribution and use, see copyright notice in Config.hpp
+
+#pragma once
+
+#ifndef NAZARA_MICROPHONE_HPP
+#define NAZARA_MICROPHONE_HPP
+
+#include <Nazara/Prerequesites.hpp>
+#include <Nazara/Audio/Config.hpp>
+
+namespace Nz
+{
+	class NAZARA_AUDIO_API Microphone
+	{
+		public:
+			Microphone() = default;
+			~Microphone() = default;
+	};
+}
+
+#endif // NAZARA_MICROPHONE_HPP
